a0b34b54966a6a66720d318458354071fee896ed
Hamza Altaf <86216890+hamza2227@users.noreply.github.com>
2022-11-29 15:07:33 +0500

    useState02

--- src/UseState_Hook.js
+++ src/UseState_Hook.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Nap from './Nap';
 
 import './UseState_Hook.css'
@@ -6,17 +6,17 @@ import './UseState_Hook.css'
 const UseState_Hook = () => {
     let [count, setCount] = useState(0);
     let [isMorning, setMorning] = useState(true);
-  return (
-    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
-        <h3>Day time = {isMorning ? 'Morning':'Night'}</h3>
+    return (
+        <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+            <h3>Day time = {isMorning ? 'Morning' : 'Night'}</h3>
 
+            {/* <h4>Value of counter is: {count}</h4> */}
+            <Nap counter={count} />
+            <button onClick={() => setCount(count + 1)}>Click</button>
 
-        <Nap counter = {count}/>
-        <button onClick={()=>setCount(count +1)}>Click</button>
-
-        <button onClick={()=>setMorning(!isMorning)}>Update Day</button>
-    </div>
-  )
+            <button onClick={() => setMorning(!isMorning)}>Update Day</button>
+        </div>
+    )
 }
 
 export default UseState_Hook
