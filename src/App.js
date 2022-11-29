9246480d40820394f04fbfde0bcef00367fa3586
Hamza Altaf <86216890+hamza2227@users.noreply.github.com>
2022-11-29 12:43:22 +0500

    useState with props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Dine from "./Dine";
 import Nap from "./Nap";
+import UseState_Hook from "./UseState_Hook";
 
 function App() {
   return (
     <div className="App">
       <Dine name = "Biryani" sweet = "Sheer"/>
-      <Nap name = "Weeping" play = "Playing"/>
+      {/* <Nap name = "Weeping" play = "Playing"/> */}
+      <UseState_Hook />
     </div>
   );
 }
--- src/Nap.js
+++ src/Nap.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Nap = (props) => {
     return (
         <div>
-            <h5>Kids are {props.name}</h5>
-            <h5>Kids are {props.play}</h5>
+            {/* <h5>Kids are {props.name}</h5>
+            <h5>Kids are {props.play}</h5> */}
+            <h4>Value of counter is: {props.counter}</h4>
         </div>
     )
 }
